69c43adaa321c02cea56702e1d38b2e099ae66f1
root <danfmihai@hotmail.com>
2021-09-27 13:34:59 -0400

    edited

--- asg.tf
+++ asg.tf
@@ -4,7 +4,7 @@ resource "aws_autoscaling_group" "dev" {
   launch_configuration = aws_launch_configuration.dev.name
   min_size             = var.min_size
   max_size             = var.max_size
-  availability_zones = [ "us-east1a","us-east-1b","us-east-1c" ]
+  availability_zones = [ "us-east-1a","us-east-1b","us-east-1c" ]
   lifecycle {
     create_before_destroy = true
   }
