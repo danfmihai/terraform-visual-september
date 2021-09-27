f57ad323f182b7cb97b645e7b25d9ba2c2afafe0
root <danfmihai@hotmail.com>
2021-09-27 14:02:20 -0400

    added 2nd one

--- subnet.tf
+++ subnet.tf
@@ -3,6 +3,15 @@ resource "aws_subnet" "dev1" {
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "dev"
+    Name = "dev1"
+  }
+}
+
+resource "aws_subnet" "dev2" {
+  vpc_id     = aws_vpc.dev.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "dev2"
   }
 }
