e06709032b0e8c7879389256786b3a5631610d88
root <danfmihai@hotmail.com>
2021-09-27 15:05:05 -0400

    added public ip address

--- lc.tf
+++ lc.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "dev" {
   name_prefix   = "dev-lc-"
   image_id      = var.ami
   instance_type = var.instance_type
-  associate_public_ip_address = true
+  associate_public_ip_address = true 
 
   lifecycle {
     create_before_destroy = true
