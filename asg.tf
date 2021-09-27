d627a891a9a13d3a9d2308f733059c49b3e37b27
root <danfmihai@hotmail.com>
2021-09-27 14:18:08 -0400

    edited

--- asg.tf
+++ asg.tf
@@ -4,8 +4,6 @@ resource "aws_autoscaling_group" "dev" {
   launch_configuration = aws_launch_configuration.dev.name
   min_size             = var.min_size
   max_size             = var.max_size
-  availability_zones = [ "us-east-1a","us-east-1b","us-east-1c" ]
-  lifecycle {
-    create_before_destroy = true
-  }
+  
+  vpc_zone_identifier = ["aws_subnet.dev1.id","aws_subnet.dev2.id"]
 }
